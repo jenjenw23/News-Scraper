cc09745e1de5391d22df43b7129fbd1310900b7c
Woulas <jwoula01@presencehealth.net>
2018-04-15 09:59:16 -0500

    read me and ui clean up, fixed modal note

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Mongo Scraper ia a web app that lets users view the latest news articles from Th
 Built with Mongo, Mongoose, Node, Express and Handlebars. 
 
 [Mongo Scraper App on Heroku](https://arcane-bastion-92075.herokuapp.com/)
+
+## App Screenshot
+![App Screenshot](https://github.com/jenjenw23/jenjenw23.github.io/blob/master/assets/images/news-scraper.jpg)
--- public/app.js
+++ public/app.js
@@ -1,9 +1,8 @@
-$('#elem').popover()
 
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", "#addnote", function() {
   // Empty the notes from the note section
-  $("#notes").empty();
+  //$("#notes").empty();
   // Save the id from the p tag
   var thisId = $(this).attr("data-id");
 
@@ -16,20 +15,21 @@ $(document).on("click", "p", function() {
     .then(function(data) {
       console.log(data);
       // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
+     // $("#notes").append("<h2>" + data.title + "</h2>");
       // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
+     // $("#notes").append("<input id='titleinput' name='title' >");
       // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      //$("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      //$("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
       // If there's a note in the article
       if (data.note) {
         // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
+      //  $("#titleinput").val(data.note.title);
         // Place the body of the note in the body textarea
         $("#bodyinput").val(data.note.body);
+        //$("#savenote").html('Update Note');
       }
     });
 });
@@ -45,7 +45,7 @@ $(document).on("click", "#savenote", function() {
     url: "/articles/" + thisId,
     data: {
       // Value taken from title input
-      title: $("#titleinput").val(),
+      //title: $("#titleinput").val(),
       // Value taken from note textarea
       body: $("#bodyinput").val()
     }
@@ -55,12 +55,12 @@ $(document).on("click", "#savenote", function() {
       // Log the response
       console.log(data);
       // Empty the notes section
-      $("#notes").empty();
+      //$("#notes").empty();
     });
 
   // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
+ // $("#titleinput").val("");
+ // $("#bodyinput").val("");
 });
 
 
